da8ad70e35abf63aa9cbdcc7107cf18c3faea5ee
Archangel Macsika <drmacsika@gmail.com>
2021-08-10 21:42:19 +0100

    Added logout functionality

---
+++ frontend/src/redux/auth/actions.js
@@ -0,0 +1,128 @@
+import {
+  SIGNUP_SUCCESS,
+  SIGNUP_FAIL,
+  SET_AUTH_LOADING,
+  REMOVE_AUTH_LOADING,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
+  RESET_SIGNUP_SUCCESS,
+} from "./types";
+
+// Action creators
+// Signup
+export const signup =
+  (first_name, last_name, username, password, re_password) =>
+  async (dispatch) => {
+    const body = JSON.stringify({
+      first_name,
+      last_name,
+      username,
+      password,
+      re_password,
+    });
+
+    dispatch({
+      type: SET_AUTH_LOADING,
+    });
+
+    try {
+      const res = await fetch("/api/accounts/signup", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: body,
+      });
+
+      if (res.status === 201) {
+        dispatch({
+          type: SIGNUP_SUCCESS,
+        });
+      } else {
+        dispatch({
+          type: SIGNUP_FAIL,
+        });
+      }
+    } catch (err) {
+      dispatch({
+        type: SIGNUP_FAIL,
+      });
+    }
+    dispatch({
+      type: REMOVE_AUTH_LOADING,
+    });
+  };
+
+export const reset_signup_success = () => (dispatch) => {
+  dispatch({
+    type: RESET_SIGNUP_SUCCESS,
+  });
+};
+
+//Login
+export const login = (username, password) => async (dispatch) => {
+  const body = JSON.stringify({
+    username,
+    password,
+  });
+
+  dispatch({
+    type: SET_AUTH_LOADING,
+  });
+
+  try {
+    const res = await fetch("/api/accounts/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: body,
+    });
+
+    if (res.status === 200) {
+      dispatch({
+        type: LOGIN_SUCCESS,
+      });
+    } else {
+      dispatch({
+        type: LOGIN_FAIL,
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+  }
+  dispatch({
+    type: REMOVE_AUTH_LOADING,
+  });
+};
+
+export const logout = () => async (dispatch) => {
+  try {
+    const res = await fetch("api/accounts/logout", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (res.status === 200) {
+      dispatch({
+        type: LOGOUT_SUCCESS,
+      });
+    } else {
+      dispatch({
+        type: LOGOUT_FAIL,
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: LOGOUT_FAIL,
+    });
+  }
+};
---
+++ frontend/src/redux/auth/reducers.js
@@ -0,0 +1,71 @@
+import {
+  SIGNUP_SUCCESS,
+  SIGNUP_FAIL,
+  SET_AUTH_LOADING,
+  REMOVE_AUTH_LOADING,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
+  RESET_SIGNUP_SUCCESS,
+} from "./types";
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+  loading: false,
+  signup_success: false,
+};
+
+const authReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        signup_success: true,
+      };
+    case SIGNUP_FAIL:
+      return {
+        ...state,
+      };
+    case RESET_SIGNUP_SUCCESS:
+      return {
+        ...state,
+        signup_success: false,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: true,
+      };
+    case LOGIN_FAIL:
+      return {
+        ...state,
+        isAuthenticated: false,
+      };
+    case LOGOUT_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: false,
+      };
+    case LOGOUT_FAIL:
+      return {
+        ...state,
+      };
+    case SET_AUTH_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case REMOVE_AUTH_LOADING:
+      return {
+        ...state,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
